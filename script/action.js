eba093dcdb3036e19588e2e5f913e5d584833462
euihyuck <euihyk12@naver.com>
2024-09-11 12:39:07 +0900

    09.11

--- script/action.js
+++ script/action.js
@@ -37,41 +37,39 @@ if (month < 10) { month = '0' + month }
 $('#section5 h2 span').text(year + '년' + month + '월' + date + '일')
 
 let suin = 0
-//BEST 그래프
+// BEST 그래프
+$('.graph li').each(function () {
+    let gra = parseInt($(this).attr('data-graphVal'))
+    if (gra > suin) {
+        suin = gra
+    }
+})
 
-let sec5 = $('section5').offset().top//2448
-
+let sec5 = $('#section5').offset().top  //2448
+let yujung = 1
 $(window).scroll(function () {
     let scrT = $(window).scrollTop();
-    if (scrT >= sec5) { graph() }
-
-    f
+    if (scrT >= sec5 && yujung == 1) {
+        graph()
+        yujung = 0
+    }
 })
-$('.graph li').each(function () {
-    let gra = parseInt($(this).attr('data-grahVal'))
-    let graVal = gar / suin * 100
 
-    /*   let liText = $(this).text().substring(0, 10)
-      $(this).find('span').text(liText)
-  
-      console.log(liText)
-   */
-    /*  $(this).height(graVal + '%') */
+function graph() {
+    $('.graph li').each(function () {
+        let gra = parseInt($(this).attr('data-graphVal'))
+        let graVal = gra / suin * 100
 
-    /*     if (gra > suin) {
-            suin = gra
-        }
-        let graVal = gra / 200 * 100
-        $(this).height(graVal + '%')
-     */
+        // let liText = $(this).text().substring(0,10)
+        // $(this).find('span').text(liText)
 
-    $(this).animate({ height: graVal + '%' }, 1000)
-})
+        $(this).animate({ height: graVal + '%' }, 1000)
+    })
+}
+
 
 $('.graph li').mouseover(function () {
     let praText = $(this).text()
     $(this).attr('title', praText)
 })
-
-
 
